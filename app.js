2cf39d5543a66acf86b3bdf5e30e4ce0ea37a593
michel jean <michel.jean.uk@gmail.com>
2021-07-02 11:14:43 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const getDate = require('./date');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 const app = express();
 const date = require(__dirname + "/date.js");
 
@@ -10,33 +12,108 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
-// var items = ['Buy Food', 'Cook Food', 'Eat Food'];
-var items = [];
-var workItems = [];
+mongoose.connect("mongodb+srv://michel92:zidane92@todolist.rmvqn.mongodb.net/myFirstDatabase?retryWrites=true/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name:"Welcome to todolist!"
+});
+const defaultItems = [item1];
+
+const listSchema = {
+    name: String, 
+    items: [itemsSchema]
+};
+const List = mongoose.model('List', listSchema);
+
 
 app.get('/', function(req, res) {
-    // let day = date.getDay(); to get the day function only
-    let day = date.getDate();
-    res.render('index', { listTitle:day, newListItems:items});
+ 
+    Item.find({}, function(err, foundItems) {
+        if(foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(error, docs) {
+                if(error) {
+                    console.log(error);
+                } else {
+                    console.log("Successfully saved default to the DB");
+                }
+            });
+            res.redirect('/');
+        } else {
+            // let day = date.getDate();
+            res.render('index', {listTitle:"Today", newListItems:foundItems});
+        }
+    });
+});
+
+app.get("/:customListName", function(req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({name: customListName}, function(err, foundList) {
+        if(!err) {
+            if(!foundList) {
+                //create a new listTitle
+                const list = new List({
+                    name: customListName,
+                    items:defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                // show existing list
+                res.render('index', {listTitle:foundList.name, newListItems:foundList.items});
+            }
+        }
+    });
 });
 
 
 app.post('/', function(req, res) {
-    let item = req.body.newItem;
-    if (req.body.index === 'Work') {
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
+    const itemName = req.body.newItem;
+    const listName = req.body.index;
+    const item = new Item({
+        name: itemName
+    });
+
+    if(listName === "Today") {
+        item.save();
         res.redirect('/');
+    } else {
+        List.findOne({name: listName}, function(err, foundList) {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
     }
 });
 
-app.get('/work', function(req, res) {
-    res.render('index', {listTitle:'Work List', newListItems:workItems});
+app.post("/delete", function(req, res) {
+    const checkedItemBody = req.body.checkBox;
+    const listName = req.body.listName;
+
+    if(listName === "Today") {
+        Item.findByIdAndRemove(checkedItemBody, function(err) {
+            if (!err) {
+                console.log("Successfully deleted checked item");
+                setTimeout(function() {
+                    res.redirect('/');
+                  }, 2000); 
+            }
+        });
+    } else {
+        List.findOneAndUpdate({name:listName}, {$pull:{items:{_id:checkedItemBody}}}, function(err, foundList) {
+            if(!err) {
+                setTimeout(function() {
+                    res.redirect('/' + listName);
+                  }, 2000); 
+            }
+        });
+    }
 });
-
-
 
 
 app.listen(3000, function() {
